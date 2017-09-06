3ffb314ad68451f96146769ad60c55d67e215813
Marii Yonov <myonov@gmail.com>
2017-09-06 13:13:31 +0300

    Add total times on the StatsComponent screen

--- src/StatsComponent.js
+++ src/StatsComponent.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {renderFinishedTasks} from './utils';
+import FontAwesome from 'react-fontawesome';
+import {formatTimeFromSeconds, renderFinishedTasks} from './utils';
 
 import './StatsComponent.css';
 
 export default function StatsComponent(props) {
+    let totalTime = 0;
+    let pausedTime = 0;
+    let runningTime = 0;
+
+    for (let finishedTask of props.finishedTasks) {
+        pausedTime += finishedTask.timeStats.pausedTime;
+        runningTime += finishedTask.timeStats.runningTime;
+        totalTime += finishedTask.timeStats.pausedTime + finishedTask.timeStats.runningTime;
+    }
+
     return <div className="container">
         <div className="finished-tasks-header">
             <h2>Finished</h2>
+            <span>
+                <FontAwesome name="angle-right"/>
+                {formatTimeFromSeconds(totalTime)}
+            </span>
+            <span>
+                <FontAwesome name="play"/>
+                {formatTimeFromSeconds(runningTime)}
+            </span>
+            <span>
+                <FontAwesome name="pause"/>
+                {formatTimeFromSeconds(pausedTime)}
+            </span>
             <h4>
                 <a className="underlined"
                    onClick={() => {
